fba3f33d86eed992478bfab9cf0dc28590efddcb
Seonghyun Kim <archun39@kookmin.ac.kr>
2025-01-16 19:23:12 +0900

    REFACTOR : 타입 변환

--- src/main/java/com/algoanalyzer/domain/problem/service/ProblemService.java
+++ src/main/java/com/algoanalyzer/domain/problem/service/ProblemService.java
@@ -1,5 +1,7 @@
 package com.algoanalyzer.domain.problem.service;
 
+import java.util.stream.Collectors;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -7,14 +9,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.algoanalyzer.domain.problem.api.dto.SolvedAcProblemResponse;
-import com.algoanalyzer.domain.problem.api.dto.SolvedAcProblemResponse.ProblemTag;
 import com.algoanalyzer.domain.problem.dto.response.ProblemResponseDto;
 import com.algoanalyzer.domain.problem.exception.ProblemNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
