e1a0330c65a569967d62d5d239e7522e29dbca3e
Seonghyun Kim <archun39@kookmin.ac.kr>
2025-01-21 20:59:51 +0900

    ADD : DB 문제 document 추가

---
+++ src/main/java/com/algoanalyzer/domain/problem/document/ProblemDocument.java
@@ -0,0 +1,27 @@
+
+package com.algoanalyzer.domain.problem.document;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Builder;
+
+@Getter
+@Builder
+@Data
+@Document(collection = "problems")  // MongoDB 컬렉션 이름
+public class ProblemDocument {
+    @Id
+    private Long problemId;
+    private String title;
+    private String description;
+    private String input;
+    private String output;
+    private String timeLimit;
+    private String memoryLimit;
+    private List<String> tags;
+} 
