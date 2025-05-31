629756cdae83f5838a3a6a79d14ed55149da22bf
Seonghyun Kim <archun39@kookmin.ac.kr>
2025-05-31 18:00:14 +0900

    FIX: problemId 외부의존성 문제

--- src/main/java/com/algoanalyzer/analysis/infrastructure/repository/ProblemAnalysisDocument.java
+++ src/main/java/com/algoanalyzer/analysis/infrastructure/repository/ProblemAnalysisDocument.java
@@ -2,6 +2,7 @@ package com.algoanalyzer.analysis.infrastructure.repository;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.index.Indexed;
 
 import lombok.Data;
 import lombok.Builder;
